58f06ca792e2dfca334be032582dbf7edc475c4c
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2025-12-01 10:26:59 +0100

    Add a generic BackLink component for getting back to job info

---
+++ core/features/jobInfos/components/JobInfoBackLink.tsx
@@ -0,0 +1,22 @@
+import { Suspense } from "react";
+
+import { BackLink } from "@/core/components/BackLink";
+import { getJobInfoById } from "../db";
+
+export default function JobInfoBackLink({ jobInfoId }: { jobInfoId: string }) {
+  return (
+    <BackLink href={`/app/job-infos/${jobInfoId}`}>
+      <Suspense fallback="Back to Job Info Details">
+        <JobInfoName jobInfoId={jobInfoId} />
+      </Suspense>
+    </BackLink>
+  );
+}
+
+async function JobInfoName({ jobInfoId }: { jobInfoId: string }) {
+  const jobInfo = await getJobInfoById(jobInfoId);
+
+  return jobInfo?.name
+    ? `Back to "${jobInfo.name}"`
+    : "Back to Job Info Details";
+}
