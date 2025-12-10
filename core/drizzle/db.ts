31c183a09ecbad176a8c97d13d329eca07e7b8bf
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2025-12-10 10:56:01 +0100

    Hide distracting logs coming from Drizzle

--- core/drizzle/db.ts
+++ core/drizzle/db.ts
@@ -4,6 +4,6 @@ import { env } from "@/core/data/env/server";
 import * as schema from "@/core/drizzle/schema";
 
 export const db = drizzle(env.DATABASE_URL, {
-  logger: true,
+  logger: false,
   schema,
 });
