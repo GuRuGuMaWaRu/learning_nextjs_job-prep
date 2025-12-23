64cf81a5df630fd54550338cf4cdfb762d85ad6a
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2025-12-23 18:47:42 +0100

    refactor: remove Onboarding page

--- app/onboarding/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-
-import { routes } from "@/core/data/routes";
-
-// Note: With the new auth system, users are created immediately on signup
-// so this onboarding page just redirects to the app
-export default function OnboardingPage() {
-  redirect(routes.app);
-}
